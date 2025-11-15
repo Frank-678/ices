d417ee2c9b50524f138d4cdfbdc28c233531b3e9
Zhu Jun <zj982659577@qq.com>
2025-11-15 20:34:27 +0800

    Improve graphs and add well-rounded comments

--- analyze.py
+++ analyze.py
@@ -1,15 +1,15 @@
-# Import libraries used in 'main' function
+# Perform sentiment analysis on user comments on Minecraft
 from csv import DictReader
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# Initialization of global variables
+# Initialize global variables
 pos = 0
 neu = 0
 neg = 0
 
 
 def main():
-    # Create variable tailored to analyze
+    # Create analyzer object
     analyzer = SentimentIntensityAnalyzer()
     
     # Read csv
@@ -17,46 +17,54 @@ def main():
         reader = DictReader(file)
         for row in reader:
             # Analyze each sample    
-            analyzer_score = analyzer.polarity_scores(row['Review_Text'])
+            analyzer_score = analyzer.polarity_scores(row['Review_Text']) # 注：返回的变量是字典，key分别是pos, neu, neg（相对权重比例）和compound（整体分数-1 ~ +1）
             classify_sentiment(analyzer_score['compound'])
     
     #  Summarize in the form of words and charts
-    print("Summary:",f"positive : {pos}",f"neutral : {neu}",f"negtive : {neg}", sep = '\n')
+    print("Summary:",f"positive : {pos}",f"neutral : {neu}",f"negative : {neg}", sep = '\n') # 注：sep指的不是 '+'（字符串连接符）而是','
     print_chart()
 
 
+# Classify
 def classify_sentiment(score):
-    
-    global pos, neu, neg
+    # State global variables
+    global pos, neu, neg  # 注解易错：Python中修改全局变量必须global声明，而读取不需要。
 
+    # Justify sentiment intensity
     if score < -0.5:
         neg += 1
     elif score > 0.5:
         pos += 1
     else:
-        neu += 1
+        neu += 1 # 注：这里用match-case可能会麻烦（case不能是不等号）
 
 
+# Print pie and bar charts
 def print_chart():
     import matplotlib.pyplot as plt
 
-    labels = ['positive', 'neutral', 'negtive']
+    labels = ['positive', 'neutral', 'negative']
     sizes = [pos, neu, neg]
 
-    plt.figure(figsize = (16, 12))
+    # Create a window 
+    plt.figure(figsize = (16, 9))
 
-    plt.subplot(8, 12, 1)
+    # Generate pie chart on the left
+    plt.subplot(1, 2, 1)  # 注：这里前两个参数表示把figure用几行几列分隔开
     plt.pie(sizes, labels=labels, autopct='%1.1f%%')
-    plt.title('Pie Chart Example')
+    plt.title('Pie Chart')
 
-    plt.subplot(8, 12, 2)
+    # Generate bar chart on the right
+    plt.subplot(1, 2, 2)
     plt.bar(labels, sizes)
-    plt.title('Bar Chart Example')
+    plt.title('Bar Chart')
     plt.xlabel('Categories')
     plt.ylabel('Values')
 
+    # Show graphs
     plt.show()
 
 
+# Execute
 if __name__ == '__main__':
     main()
